5221f19ed2b7fa11917e1b98981b5364c0535061
User <0332221039@unjfsc.edu.pe>
2026-09-15 08:47:39 -0500

    corregir consulta de recetas por paciente

--- src/main/java/com/lvl/medicenter/repository/RecetaDetalleRepository.java
+++ src/main/java/com/lvl/medicenter/repository/RecetaDetalleRepository.java
@@ -2,9 +2,22 @@ package com.lvl.medicenter.repository;
 
 import com.lvl.medicenter.domain.RecetaDetalle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RecetaDetalleRepository extends JpaRepository<RecetaDetalle, Integer> {
+
     List<RecetaDetalle> findByAtencionId(Integer atencionId);
+
+    @Query(value = """
+        SELECT rd.*
+        FROM MC_RECETA_DETALLE rd
+        JOIN MC_ATENCION_MEDICA am
+            ON rd.ATENCION_ID = am.ATENCION_ID
+        JOIN MC_CITA c
+            ON am.CITA_ID = c.CITA_ID
+        WHERE c.PACIENTE_ID = ?1
+        """, nativeQuery = true)
+    List<RecetaDetalle> findByPacienteId(Integer pacienteId);
 }
